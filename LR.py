b8680b4eb538e359c6ac2f1f4ca659ebc7f17083
Shuming Fang <fangshuming519@gmail.com>
2018-01-02 12:48:34 +0800

    Update LR.py

--- LR.py
+++ LR.py
@@ -1,18 +1,17 @@
 import numpy as np
 import tensorflow as tf
-X = np.arange(100)
+X = np.arange(10000)
 X = X.astype('float32')
-Y = 2*X + 0.1*np.random.randn(100)
+Y = 2*X + 0.1*np.random.randn(10000)
 Y = Y.astype('float32')
 x  = tf.placeholder(tf.float32, name = 'x')
 y = tf.placeholder(tf.float32, name = 'y')
 w = tf.Variable(0.0, name = 'weights')
 yp = w * x
 loss = tf.reduce_mean(tf.abs(y - yp), name = 'loss')
-optimizer = tf.train.AdamOptimizer(learning_rate = 0.001).minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate = 0.01).minimize(loss)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for i in range(10000):
+    for i in range(10):
         result = sess.run(optimizer, feed_dict = {x:X, y:Y})
-        print(sess.run(w))
-
+        print('w:	', sess.run(w), '	loss:	', sess.run(loss, feed_dict = {x:X, y:Y}))
