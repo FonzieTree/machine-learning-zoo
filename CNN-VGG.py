9aff8d87f503d6ca9953f0a005a01d988e769747
Shuming Fang <fangshuming519@gmail.com>
2018-01-16 11:47:30 +0800

    Update and rename CNN.py to CNN-VGG.py

--- CNN-VGG.py
+++ CNN-VGG.py
@@ -1,4 +1,5 @@
 # Data source: https://www.kaggle.com/c/digit-recognizer/data
+# Net source: http://cs231n.github.io/convolutional-networks/
 import numpy as np
 import tensorflow as tf
 np.random.seed(1)
